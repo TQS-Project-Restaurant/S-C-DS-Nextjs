031b381b223755c0136e0bf8255348019430b78a
WildBunnie <wildbunnie2003@gmail.com>
2024-05-23 17:37:40 +0100

    fixed code smells. replaced table with divs

--- app/src/app/pratos/[prato]/page.tsx
+++ app/src/app/pratos/[prato]/page.tsx
@@ -11,37 +11,35 @@ export const COMIDA_SAMPLE: Comida = {
   imagemUrl: "https://portuguesefood.pt/wp-content/uploads/2018/08/francesinha-porto-3.jpg",
 };
 
-export default function Page({ params }: { params: { prato: number } }) {
+export default function Page({ params }: { params: { prato: Readonly<number> } }) {
   return (
     <div className="relative">
       <img
+        alt="wallpaper"
         src="https://www.desktopbackground.org/download/o/2013/05/31/584855_black-chalkboard-wallpapers-walldevil-best-free-hd-desktop-and_1920x1080_h.jpg"
         className="h-[110%] w-full fixed"
       />
       <div className="text-white fixed w-full flex flex-col items-center">
         <div className="text-7xl text-center w-full p-3">{COMIDA_SAMPLE.nome}</div>
         <img
+          alt={COMIDA_SAMPLE.nome}
           className="text-3xl py-3 pt-4 pb-2 w-[400px] h-[300px] object-cover"
           src={COMIDA_SAMPLE.imagemUrl}
         />
         <div className="text-5xl text-center w-full p-3">€{COMIDA_SAMPLE.preco.toFixed(2)}</div>
-        <div className="w-[400px] flex justify-center pt-4">
-          <table className="text-white text-xl bg-black bg-opacity-50 border border-gray-300 w-full">
-            <tbody>
-              <tr>
-                <td className="p-2 border border-gray-300">Kcal</td>
-                <td className="p-2 border border-gray-300">{COMIDA_SAMPLE.kcal}</td>
-              </tr>
-              <tr>
-                <td className="p-2 border border-gray-300">Hidratos de Carbono</td>
-                <td className="p-2 border border-gray-300">{COMIDA_SAMPLE.hidratosCarbono}g</td>
-              </tr>
-              <tr>
-                <td className="p-2 border border-gray-300">Proteína</td>
-                <td className="p-2 border border-gray-300">{COMIDA_SAMPLE.proteina}g</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="w-[400px] flex flex-col items-center pt-4">
+          <div className="w-full flex justify-between p-2 bg-black bg-opacity-50 border border-gray-300 rounded-t-md">
+            <div className="p-2">Kcal</div>
+            <div className="p-2">{COMIDA_SAMPLE.kcal}</div>
+          </div>
+          <div className="w-full flex justify-between p-2 bg-black bg-opacity-50 border border-gray-300">
+            <div className="p-2">Hidratos de Carbono</div>
+            <div className="p-2">{COMIDA_SAMPLE.hidratosCarbono}g</div>
+          </div>
+          <div className="w-full flex justify-between p-2 bg-black bg-opacity-50 border border-gray-300 rounded-b-md">
+            <div className="p-2">Proteína</div>
+            <div className="p-2">{COMIDA_SAMPLE.proteina}g</div>
+          </div>
         </div>
       </div>
     </div>
