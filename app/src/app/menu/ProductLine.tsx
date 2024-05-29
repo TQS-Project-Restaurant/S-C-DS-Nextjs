ef30a38769678af129cb014f1fe5250e1a2dfb0b
WildBunnie <wildbunnie2003@gmail.com>
2024-05-29 11:05:06 +0100

    added bebida page and tests. also changed menu to route properly to the bebidas page. added a button to go back to menu as well

--- app/src/app/menu/ProductLine.tsx
+++ app/src/app/menu/ProductLine.tsx
@@ -3,15 +3,27 @@
 import { useRouter } from "next/navigation";
 import { Comida } from "../_interfaces";
 
-interface ComidaProps{
-    comida:Comida,
+interface ComidaProps {
+  comida: Comida;
+  isBebida: boolean;
 }
 
-export default function ProductLine({ comida }:Readonly<ComidaProps>) : JSX.Element{
-    const router = useRouter();
-    return(
-        <button data-testid="prato" onClick={()=>router.push(`/pratos/${comida.id}`)} className="px-2 w-full text-center text-2xl flex justify-between">
-            <div>{comida.nome}</div> {comida.preco} €
-        </button>
-    )
+export default function ProductLine({ comida, isBebida }: ComidaProps): JSX.Element {
+  const router = useRouter();
+
+    if (isBebida) {
+        return (
+            <button data-testid="prato" onClick={()=>router.push(`/bebidas/${comida.id}`)} className="px-2 w-full text-center text-2xl flex justify-between">
+              <div>{comida.nome}</div> {comida.preco} €
+            </button>
+          );    
+    } else {
+        return (
+            <button data-testid="prato" onClick={()=>router.push(`/pratos/${comida.id}`)} className="px-2 w-full text-center text-2xl flex justify-between">
+                <div>{comida.nome}</div> {comida.preco} €
+            </button>
+        );    
+    }
+
+
 }
