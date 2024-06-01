7b6cd21f5c2bc52a18eeae64bf8a1a6669a89cdb
Tomas Victal <tomasvictal@gmail.com>
2024-06-01 19:21:36 +0100

    added login to add and a navbar

--- app/src/app/pedidos/page.tsx
+++ app/src/app/pedidos/page.tsx
@@ -1,23 +1,35 @@
 "use client";
+import { useSession } from "next-auth/react";
 import { Status,Pedido } from "../_interfaces";
 import Ticket from "./Ticket";
 import useSWR from "swr";
+import { redirect } from "next/navigation";
 
-async function fetcher<Pedido>(url:string):Promise<Pedido[]>{
-  const res = await fetch(url,{cache:"no-store"});
+async function fetcher<Pedido>(url:string,token:any):Promise<Pedido[]>{
+  const res = await fetch(url,{
+    headers:{Authorization: `Bearer ${token}`}
+  });
   if(!res.ok){
-    throw new Error("Cant acess data");
+    throw new Error("Error fetching data");
   }
   return(res.json())
 }
 
 
 export default function Pedidos() {
-  const {data,error} = useSWR<Pedido[]>("http://localhost:8080/api/requests",fetcher,{refreshInterval:5000});
+  const { data: session } = useSession({
+    required:true,
+    onUnauthenticated(){
+      redirect('/api/auth/signin?callbackUrl=/signage')
+    }
+  })
+  if(session?.user.role !== "KITCHEN")
+    redirect("/menu")
+  const {data , error} = useSWR<Pedido[]>(session? ["http://localhost:8080/api/requests",session.user.token]:null,([url,token])=>fetcher(url,token),{refreshInterval:5000});
   if (error) return <div>Erro ao carregar os dados.</div>;
   if (!data) return <div>Carregando...</div>;
     return (
-      <div className="h-[90%] grid grid-cols-3 p-4 gap-2">
+      <div className="grid grid-cols-3 p-4 gap-2 flex-grow">
         <div className=" bg-gray-300 rounded-md text-black p-2 overflow-scroll">
             <div className=" font-bold text-2xl p-2">Pending:</div>
             <div className="grid grid-cols-3 gap-3 gap-y-6">
