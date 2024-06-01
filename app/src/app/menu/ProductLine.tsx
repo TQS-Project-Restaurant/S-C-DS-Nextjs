5c249804d669f50cc9ad31d5123086325be75261
WildBunnie <wildbunnie2003@gmail.com>
2024-06-01 15:49:20 +0100

    changed if statemente in product line

--- app/src/app/menu/ProductLine.tsx
+++ app/src/app/menu/ProductLine.tsx
@@ -4,26 +4,19 @@ import { useRouter } from "next/navigation";
 import { Comida } from "../_interfaces";
 
 interface ComidaProps {
-  comida: Comida;
-  isBebida: boolean;
+    comida: Comida;
+    isBebida: boolean;
 }
 
 export default function ProductLine({ comida, isBebida }: Readonly<ComidaProps>): JSX.Element {
-  const router = useRouter();
+    const router = useRouter();
 
-    if (isBebida) {
-        return (
-            <button data-testid="prato" onClick={()=>router.push(`/bebidas/${comida.id}`)} className="px-2 w-full text-center text-2xl flex justify-between">
-              <div>{comida.nome}</div> {comida.preco} €
-            </button>
-          );    
-    } else {
-        return (
-            <button data-testid="prato" onClick={()=>router.push(`/pratos/${comida.id}`)} className="px-2 w-full text-center text-2xl flex justify-between">
-                <div>{comida.nome}</div> {comida.preco} €
-            </button>
-        );    
-    }
+    const url = isBebida ? `/bebidas/${comida.id}`:`/pratos/${comida.id}`;
 
+    return (
+        <button data-testid="prato" onClick={()=>router.push(url)} className="px-2 w-full text-center text-2xl flex justify-between">
+            <div>{comida.nome}</div> {comida.preco} €
+        </button>
+    );
 
 }
