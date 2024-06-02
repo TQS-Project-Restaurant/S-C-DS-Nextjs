969b1100b0948fcaeb59ea28e34ebf44e7fee3f6
WildBunnie <wildbunnie2003@gmail.com>
2024-06-02 18:52:24 +0100

    refresh page after making reservation and added minimum date to date picker

--- app/src/app/reservas/page.tsx
+++ app/src/app/reservas/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 
 type Slot = [number, number];
 
-export default function BookingPage() {
+export default function ReservasPage() {
   const [date, setDate] = useState<string>("");
   const [slots, setSlots] = useState<Slot[]>([]);
   const [selectedSlot, setSelectedSlot] = useState<number[]>([]);
@@ -55,32 +55,42 @@ export default function BookingPage() {
 
     if (response.status === 201) {
       alert("Booking successful");
+      fetchAvailableSlots(date);
     } else {
       console.error("Error making booking");
       alert("Booking failed");
     }
   };
 
+  const getTodayDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Book a Slot</h1>
       <div className="mb-4">
-        <label className="block text-gray-700">Choose a date:</label>
+        <label className="block text-gray-700">Choose a date:
         <input
           type="date"
           value={date}
           onChange={handleDateChange}
           onClick={(e) => e.currentTarget.showPicker()}
+          min={getTodayDate()}
           className="mt-1 block px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
         />
+        </label>
       </div>
       {slots.length > 0 && (
         <div className="mb-4">
-          <label className="block text-gray-700">Available Slots:</label>
+          <label className="block text-gray-700">Available Slots:
           <div className="grid grid-cols-3 gap-2 mt-2">
             {slots.map((slot, index) => (
               <button
-                key={index}
                 onClick={() => setSelectedSlot([slot[0], slot[1]])}
                 className={`px-4 py-2 rounded-md ${
                   selectedSlot[0] === slot[0] && selectedSlot[1] === slot[1]
@@ -92,10 +102,11 @@ export default function BookingPage() {
               </button>
             ))}
           </div>
+          </label>
         </div>
       )}
       <div className="mb-4">
-        <label className="block text-gray-700">Number of Tables:</label>
+        <label className="block text-gray-700">Number of Tables:
         <select
           value={tables}
           onChange={(e) => setTables(parseInt(e.target.value))}
@@ -107,6 +118,7 @@ export default function BookingPage() {
             </option>
           ))}
         </select>
+        </label>
       </div>
       <button
         onClick={handleBooking}
